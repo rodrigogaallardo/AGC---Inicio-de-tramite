323af62a0c68263cbbf94545e12d786f8dc3fa22
luis espindola <luisesp27@live.com.ar>
2023-04-25 08:51:11 -0300

    Todos los web.configs estaban apuntando a test

--- Azrael/frmReportes/frmPDFObleaTransmision.cs
+++ Azrael/frmReportes/frmPDFObleaTransmision.cs
@@ -182,7 +182,6 @@ namespace Azrael.frmReportes
                         {
                             ExternalServiceReporting service = new ExternalServiceReporting();
                             ReportingEntity Report = service.GetPDFObleaTransmision(item.IdSolicitud, true);
-                            //azBL.GuardarPDFOblea(item.IdSolicitud, Report.Id_file, Report.FileName, item.CreateUser);
                             azBL.SetNewObleaTF(item.IdSolicitud, item.CreateUser, Report.Id_file, Report.FileName);
                             ContProcesados += 1;
                             backgroundWorker1.ReportProgress(ContProcesados);
