09841c4c05b74a59178665ff7660a72949a1c5ce
luis espindola <luisesp27@live.com.ar>
2023-05-24 19:31:45 -0300

    Elimine el linq comentado y agregue el campo de fecha donde iba

--- BaseRepository/SGITareaCalificarObsGrupoRepository.cs
+++ BaseRepository/SGITareaCalificarObsGrupoRepository.cs
@@ -27,36 +27,6 @@ namespace BaseRepository
         /// <returns></returns>	
         public IEnumerable<SGITareaCalificarObsGrupoGrillaEntity> GetByFKIdSolicitud(int id_solicitud)
         {
-            /*
-            var intermediateDomains = (from obs in _unitOfWork.Db.SGI_Tarea_Calificar_ObsGrupo
-                                       join tth in _unitOfWork.Db.SGI_Tramites_Tareas_HAB on obs.id_tramitetarea equals tth.id_tramitetarea
-                                       join asp in _unitOfWork.Db.aspnet_Users on obs.CreateUser equals asp.UserId
-                                       join usr_sgi in _unitOfWork.Db.SGI_Profiles on asp.UserId equals usr_sgi.userid
-                                       join obsDoc in _unitOfWork.Db.SGI_Tarea_Calificar_ObsDocs on obs.id_ObsGrupo equals obsDoc.id_ObsGrupo
-                                       where tth.id_solicitud == id_solicitud
-                                       && tth.id_tramitetarea <= (from ha in _unitOfWork.Db.SGI_Tramites_Tareas_HAB
-                                                                  join ta in _unitOfWork.Db.SGI_Tramites_Tareas on ha.id_tramitetarea equals ta.id_tramitetarea
-                                                                  where ha.id_solicitud == id_solicitud
-                                                                  && ta.ENG_Tareas.cod_tarea.ToString().Contains("25")
-                                                                  select ha.id_tramitetarea).Max()
-                                        select new SGITareaCalificarObsGrupoGrillaEntity
-                                        {
-                                            id_tramitetarea = tth.id_tramitetarea,
-                                            id_ObsGrupo = obs.id_ObsGrupo,
-                                            CreateDate = obs.CreateDate,
-                                            LastUpdateDate = tth.SGI_Tramites_Tareas.FechaInicio_tramitetarea,
-                                            userApeNom = usr_sgi.Apellido + ", " + usr_sgi.Nombres
-                                        }
-                                       );
-        
-
-            var sig_correccionSol = (from tt in _unitOfWork.Db.SGI_Tramites_Tareas
-                                     join hab in _unitOfWork.Db.SGI_Tramites_Tareas_HAB on tt.id_tramitetarea equals hab.id_tramitetarea
-                                     join tskr in _unitOfWork.Db.ENG_Tareas on tt.id_tarea equals tskr.id_tarea
-                                     join users in _unitOfWork.Db.aspnet_Users on tt.UsuarioAsignado_tramitetarea equals users.UserId
-                                     where hab.id_solicitud == id_solicitud && hab.id_tramitetarea >= intermediateDomains.FirstOrDefault().id_tramitetarea && tskr.cod_tarea.ToString().Contains("25")
-                                     select tt.FechaInicio_tramitetarea).Min();
-            */
             var domains = (from obs in _unitOfWork.Db.SGI_Tarea_Calificar_ObsGrupo
                            join tth in _unitOfWork.Db.SGI_Tramites_Tareas_HAB on obs.id_tramitetarea equals tth.id_tramitetarea
                            join asp in _unitOfWork.Db.aspnet_Users on obs.CreateUser equals asp.UserId 
