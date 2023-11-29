deb801b6abdf67fc7d19e4e2acb1657a255dd793
luis espindola <luisesp27@live.com.ar>
2023-11-29 17:00:57 -0300

    HotFix error22

--- SSIT/Common/Functions.cs
+++ SSIT/Common/Functions.cs
@@ -290,6 +290,8 @@ namespace SSIT.Common
 
             var solicitante = lstParticipantesSSIT.FirstOrDefault(x => x.idPerfil == idPerfilSol);
 
+            var solicitanteGP = lstParticipantesGP.FirstOrDefault( x => x.idPerfil == idPerfilSol);
+
             var titular = lstParticipantesSSIT.FirstOrDefault(x => x.idPerfil == idPerfilTit);
 
             var listTitularesComplementariosCuit = lstParticipantesSSIT
@@ -297,7 +299,7 @@ namespace SSIT.Common
                                     .Select(x => x.cuit)
                                     .ToList();
             //esto para arreglar el backlog de error22
-            if (solicitante == null)
+            if (solicitanteGP == null)
             {
                 Exception ex22 = new Exception(
                     $"Debe tener solicitante para poder tramitar, titular {titular}," +
@@ -409,6 +411,8 @@ namespace SSIT.Common
 
             var solicitante = lstParticipantesSSIT.FirstOrDefault(x => x.idPerfil == idPerfilSol);
 
+            var solicitanteGP = lstParticipantesGP.FirstOrDefault(x => x.idPerfil == idPerfilSol);
+
             var titular = lstParticipantesSSIT.FirstOrDefault(x => x.idPerfil == idPerfilTit);
 
             var listTitularesComplementariosCuit = lstParticipantesSSIT
@@ -416,7 +420,7 @@ namespace SSIT.Common
                                     .Select(x => x.cuit)
                                     .ToList();
             //esto para arreglar el backlog de error22
-            if (solicitante == null)
+            if (solicitanteGP == null)
             {
                 Exception ex22 = new Exception(
                     $"Debe tener solicitante para poder tramitar, titular {titular}," +
