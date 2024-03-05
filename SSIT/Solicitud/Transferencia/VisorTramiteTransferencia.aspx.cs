8199cbca600f93c468a06ddc9bb819db9abf3cb7
Rodrigo gallardo <rgallar17@outlook.com.ar>
2024-03-05 14:22:05 -0300

    Ticket 2289 - Fix Transferencias - Error generico en listado de documentos del ssit

--- SSIT/Solicitud/Transferencia/VisorTramiteTransferencia.aspx.cs
+++ SSIT/Solicitud/Transferencia/VisorTramiteTransferencia.aspx.cs
@@ -404,8 +404,10 @@ namespace SSIT
 
         protected void btnConfirmarTramite_Click(object sender, EventArgs e)
         {
+            
             try
             {
+                Cargar();
                 var sol = TransferenciaBL.Single(IdSolicitud);
                 if (sol.idTAD != null)
                 {
@@ -423,7 +425,6 @@ namespace SSIT
                 lblError.Text = ex.Message;
                 ScriptManager.RegisterStartupScript(updEstadoSolicitud, updEstadoSolicitud.GetType(), "showfrmError", "showfrmError();", true);
             }
-            Cargar();
         }
 
         private void enviarCambio(TransferenciasSolicitudesDTO sol)
